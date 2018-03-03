87613135d65a2222e93aa7458ef9131131b44679
shkkmo <shkkmo@gmail.com>
2018-03-03 13:29:55 -0800

    Improving lastseen command

--- commands/lastseen.js
+++ commands/lastseen.js
@@ -7,57 +7,53 @@ exports.run = async (client, message, args, level) => {
         table = require('easy-table');
   var hasData=false,
       scoreTable = new table,
-      searchObj = message.guild,
-      targetID = message.author.id,
-      singleTarget = true,
-      targetDB = message.userDB;
+      guildDB = message.guild,
+      members = [],
+      errors = '';
 
   args.forEach(function(arg) {
     if (arg.indexOf("<@&") >= 0) { //target is a ROLE
       singleTarget = false;
       const roleID = arg.replace("<@&","").replace(">","");
-      if (!message.guild.roles.has(roleID)) return message.reply("Role not found! Maybe i can't mention it...");
-      searchObj = message.guild.roles.get(roleID);
+      if (!message.guild.roles.has(roleID)) {
+        errors += "Role not found! Maybe i can't mention it...\n";
+      message.guild.roles.get(roleID).members.forEach(function(targetDB, targetID){
+        members[targetID] = targetDB;
+      });
     }
     else if (arg.indexOf("<@") >= 0 ) { //target is a USER
-      singleTarget = true;
-      targetID = arg.replace("<@","").replace(">","");
-      targetDB = client.userDB.get(targetID) || {username: arg}
-    }
-    else if (arg === "all"){
-      singleTarget = false;
+      var targetID = arg.replace("<@","").replace(">","");
+      var targetDB = client.userDB.get(targetID) || {username: targetID, lastSeen: false}
+      if (!targetDB.lastSeen) {
+        errors += `I have never seen ${targetDB.username}.\n`;
+        return; //Skip to next members of args
+      }
+      if (message.author.id === targetID) {
+        errors += "Do you need a mirror ???\n";
+      }
+      members[targetID] = targetDB;
     }
 
   });
 
-  if (!singleTarget) {
-    searchObj.members.forEach(function (target, targetID, mapObj){
-      if (targetID != process.env.DISCORD_BOT_ID) {
-        targetDB = targetDB = client.userDB.get(targetID) || {username: `<@${targetID}>`}
-        if (targetDB.lastSeen) {
-          hasData=true;
-          let timeDiff = Math.round((Date.now() - targetDB.lastSeen) / 3600000);
-          scoreTable.cell('User', targetDB.username);
-          let lastSeen = timeDiff ? `${timeDiff} hours ago` : "just now...";
-          //if(targetDB.timeOffset) lastSeen += " at "+moment(Date.now() + (targetDB.timeOffset * 3600000)).format("MMM-DD, HH:mm");
-          scoreTable.cell('LastSeen', lastSeen);
-          scoreTable.newRow();
-        }
+  searchObj.members.forEach(function (targetDB, targetID, mapObj){
+    if (targetID != process.env.DISCORD_BOT_ID) {
+      targetDB = client.userDB.get(targetID) || {lastSeen: false}
+      if (targetDB.lastSeen) {
+        let timeDiff = Math.round((Date.now() - targetDB.lastSeen) / 360000) / 10; //One 10 outside the round() call so we have a single decimal
+        scoreTable.cell('User', targetDB.username);
+        let lastSeenString = timeDiff ? `${timeDiff} hours ago` : "just now...";
+        //if(targetDB.timeOffset) lastSeen += " at "+moment(Date.now() + (targetDB.timeOffset * 3600000)).format("YY-MM-DD, HH:mm");
+        scoreTable.cell('LastSeen', timeAgo);
+        scoreTable.cell('timestamp', targetDB.lastSeen, function(){return '';}) //This empty printer callback with will hide this column and give it zero width
+        if (targetDB.timeOffset)
+          scoreTable.cell('LocalTime', moment(Date.now() + (targetDB.timeOffset * 3600000)).format("MMM DD, HH:mm"));
+        scoreTable.newRow();
       }
-    });  
-    if (!hasData) return message.reply("No data found");
-    else return message.reply(`Last seen time for everyone of ${args[0] || ""} ${searchObj.name}:\n` + "```" + scoreTable.sort('User|des').toString()+"```"); 
-  }
-
-  if (message.author.id === targetID)
-    return message.reply("Do you need a mirror ???");
-  
-  if (!targetDB.lastSeen) return message.reply(`I have never seen ${targetDB.username}.`);
-  let timeDiff = Math.round((Date.now() - targetDB.lastSeen) / 3600000);
-  if (targetDB.timeOffset)
-    message.reply(`${targetDB.username} was last seen ${timeDiff} hours ago, at: `+moment(Date.now() + (targetDB.timeOffset * 3600000)).format("MMM-DD, HH:mm")+" , his local time.");
-  else
-    message.reply(`${targetDB.username} was last seen ${timeDiff} hours ago`);
+    }
+  });  
+  if (!scoreTable.rows.length) return message.reply(errors+"No data found");
+  else return message.reply(`${errors}Last seen time for everyone of ${args.join(', ')}:\n` + "```" + scoreTable.sort('timestamp|des').toString()+"```"); 
 
 };
                
