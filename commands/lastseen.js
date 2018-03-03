74b3e17308b924da2f2b7d9288924232ae1577f0
shkkmo <shkkmo@gmail.com>
2018-03-03 15:48:09 -0800

    switching comparison result

--- commands/lastseen.js
+++ commands/lastseen.js
@@ -76,7 +76,7 @@ exports.run = async (client, message, args, level) => {
   if (!scoreTable.rows.length) return message.reply(errors+"No data found");
   //else return message.reply(`${errors}Last seen time for everyone of ${args.join(', ')}:\n` + "```" + scoreTable.sort('timestamp|des').toString()+"```"); 
   else return message.reply(`Last seen time for everyone of ${args.join(', ')}:\n` + "```" + scoreTable.sort(function(a,b){
-    var result = a[''] > b[''] ? 1 : -1;
+    var result = a[''] > b[''] ? -1 : 1;
     errors += `Comparing ${a['']} to ${b['']} got ${result} \n`;
     return result; //a[''] > b[''] ? 1 : -1;
   }).toString()+"```"+`\n${errors}`); 
