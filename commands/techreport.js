12bd4c858c772ec581c6682bcd6346fc7e7e3c7a
shkkmo <shkkmo@gmail.com>
2018-03-03 22:55:53 -0800

    debugging

--- commands/techreport.js
+++ commands/techreport.js
@@ -119,7 +119,7 @@ exports.run = async (client, message, args, level) => {
             return errors += "Expected end to reportTables iteration\n";
           }
           currentTable = nextVal.value;
-          currentTable.cell('name',targetDB.username, val => String(val).substr(0,13));//Math.min(13,String(val).length);
+          //currentTable.cell('name',targetDB.username, val => String(val).substr(0,13));//Math.min(13,String(val).length);
         }
         //errors += `Processing ${techID} for memberID ${targetID}\n`; // Debug
         let techLevel = Number( allTech[techID] ) || 0;
@@ -128,9 +128,9 @@ exports.run = async (client, message, args, level) => {
         } else {
           errors += `Invalid techID ${techID}\n`;
         }
-        currentTable.cell(techLabel, techLevel);
+        //currentTable.cell(techLabel, techLevel);
       });// End techMap.forEach
-      reportTables.forEach( (currentTable, index) => { 
+      reportTables.forEach( (currentTable) => { 
         currentTable.cell('score', techScore); 
         currentTable.newRow();
       });// End reportTables.forEach
