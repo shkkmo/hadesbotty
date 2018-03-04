f1ffed7521d161eaf3a7f7e8902558222ea26708
shkkmo <shkkmo@gmail.com>
2018-03-04 01:17:41 -0800

    debug

--- commands/techreport.js
+++ commands/techreport.js
@@ -151,7 +151,7 @@ exports.run = async (client, message, args, level) => {
   if (reports.length < 1) {
     return message.reply(`${errors}No data found.`);
   } else { 
-    message.reply(`Tech Reports:\n${errors}${reportsContent}`);
+    message.reply(`Tech Reports:\n${errors}`);
     let reportsContent = reports
       .map( reportTables  =>  //get the report texts
          Array.from(reportTables.values())  //print all tables for report
