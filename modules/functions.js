cc2afc8be46d6a771e5e73cb292f22ec526c3533
Glitch (hadesbotty-testy) <none>
2018-03-05 18:11:50 +0000

    👻😨 Checkpoint modules/functions.js:663535/173

--- modules/functions.js
+++ modules/functions.js
@@ -151,7 +151,11 @@ module.exports = (client) => {
     // to-do: same thing for targetDB
     if (client.userDB.get(message.author.id)) {
       message.userDB = client.userDB.get(message.author.id);
-      message.userDB.username = message.author.username;
+      //message.userDB.username = message.author.username;
+      message.userDB.username = message.guild.fetchMember(message.author).displayName
+        .then(console.log)
+        .catch(console.error);
+
       message.userDB.lastSeen = Date.now();
     }
     else
